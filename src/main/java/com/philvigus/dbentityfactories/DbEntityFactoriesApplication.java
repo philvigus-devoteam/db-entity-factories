4867a9a4de1f96117ec0d0f9c8de9175e082498f
Phil Vigus <phil.vigus@devoteam.com>
2022-11-15 14:44:08 +0100

    Create skeleton project

---
+++ src/main/java/com/philvigus/dbentityfactories/DbEntityFactoriesApplication.java
@@ -0,0 +1,13 @@
+package com.philvigus.dbentityfactories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbEntityFactoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbEntityFactoriesApplication.class, args);
+	}
+
+}
