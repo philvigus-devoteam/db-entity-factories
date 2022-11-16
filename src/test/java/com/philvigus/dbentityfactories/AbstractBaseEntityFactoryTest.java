c1309bc1fe4775c24d25ffb8b283d66f3f026e83
Phil Vigus <phil.vigus@devoteam.com>
2022-11-16 09:40:44 +0100

    Refactor tests

--- src/test/java/com/philvigus/dbentityfactories/AbstractBaseEntityFactoryTest.java
+++ src/test/java/com/philvigus/dbentityfactories/AbstractBaseEntityFactoryTest.java
@@ -4,6 +4,7 @@ import com.philvigus.dbentityfactories.testfixtures.entities.BasicEntity;
 import com.philvigus.dbentityfactories.testfixtures.factories.BasicEntityFactory;
 import com.philvigus.dbentityfactories.testfixtures.repositories.BasicEntityRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,80 +18,58 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
 class AbstractBaseEntityFactoryTest {
+    BasicEntityFactory basicEntityFactory;
+
     @Autowired
     BasicEntityRepository basicEntityRepository;
 
+    @BeforeEach
+    void setUp() {
+        basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
+    }
+
     @Test
     void makeCanReturnASingleBasicEntityWithItsAttributesCorrectlySet() {
-        BasicEntityFactory basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
         BasicEntity basicEntity = basicEntityFactory.make();
 
-        assertTrue(basicEntity instanceof BasicEntity);
-        assertNull(basicEntity.getId());
-        assertTrue(basicEntity.getMyLongAttribute() instanceof Long);
-        assertTrue(basicEntity.getMyStringAttribute() instanceof String);
-
+        assertBasicEntityCorrectlyMade(basicEntity);
     }
 
     @Test
     void makeCanReturnAListOfMultipleBasicEntitiesWithTheirAttributesCorrectlySet() {
-        BasicEntityFactory basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
-        List<BasicEntity> basicEntities = basicEntityFactory.make(2);
+        final int numberOfEntities = 2;
 
-        assertEquals(2, basicEntities.size());
+        List<BasicEntity> basicEntities = basicEntityFactory.make(numberOfEntities);
 
-        assertTrue(basicEntities.get(0) instanceof BasicEntity);
-        assertNull(basicEntities.get(0).getId());
-        assertTrue(basicEntities.get(0).getMyLongAttribute() instanceof Long);
-        assertTrue(basicEntities.get(0).getMyStringAttribute() instanceof String);
-
-        assertTrue(basicEntities.get(1) instanceof BasicEntity);
-        assertNull(basicEntities.get(1).getId());
-        assertTrue(basicEntities.get(1).getMyLongAttribute() instanceof Long);
-        assertTrue(basicEntities.get(1).getMyStringAttribute() instanceof String);
+        assertEquals(numberOfEntities, basicEntities.size());
+        assertBasicEntityCorrectlyMade(basicEntities.get(0));
+        assertBasicEntityCorrectlyMade(basicEntities.get(1));
     }
 
     @Test
     void makeThrowsAnExceptionIfCopiesIsLessThanOne() {
-        BasicEntityFactory basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
-
         Assertions.assertThrows(IllegalArgumentException.class, () -> basicEntityFactory.make(0));
     }
 
     @Test
     void createCanReturnAndSaveASingleBasicEntityWithItsAttributesCorrectlySetToTheDatabase() {
-        BasicEntityFactory basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
         BasicEntity basicEntity = basicEntityFactory.create();
-
-        assertTrue(basicEntity instanceof BasicEntity);
-        assertTrue(basicEntity.getId() instanceof Long);
-        assertTrue(basicEntity.getMyLongAttribute() instanceof Long);
-        assertTrue(basicEntity.getMyStringAttribute() instanceof String);
-
         List<BasicEntity> savedEntities = basicEntityRepository.findAll();
 
+        assertBasicEntityCorrectlyCreated(basicEntity);
+        
         assertEquals(1, savedEntities.size());
         assertTrue(savedEntities.contains(basicEntity));
     }
 
     @Test
     void createCanReturnAndSaveMultipleBasicEntitiesWithTheirAttributesCorrectlySetToTheDatabase() {
-        BasicEntityFactory basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
         List<BasicEntity> basicEntities = basicEntityFactory.create(2);
+        List<BasicEntity> savedEntities = basicEntityRepository.findAll();
 
         assertEquals(2, basicEntities.size());
-
-        assertTrue(basicEntities.get(0) instanceof BasicEntity);
-        assertTrue(basicEntities.get(0).getId() instanceof Long);
-        assertTrue(basicEntities.get(0).getMyLongAttribute() instanceof Long);
-        assertTrue(basicEntities.get(0).getMyStringAttribute() instanceof String);
-
-        assertTrue(basicEntities.get(1) instanceof BasicEntity);
-        assertTrue(basicEntities.get(1).getId() instanceof Long);
-        assertTrue(basicEntities.get(1).getMyLongAttribute() instanceof Long);
-        assertTrue(basicEntities.get(1).getMyStringAttribute() instanceof String);
-
-        List<BasicEntity> savedEntities = basicEntityRepository.findAll();
+        assertBasicEntityCorrectlyCreated(basicEntities.get(0));
+        assertBasicEntityCorrectlyCreated(basicEntities.get(1));
 
         assertEquals(2, savedEntities.size());
         assertTrue(savedEntities.contains(basicEntities.get(0)));
@@ -99,8 +78,20 @@ class AbstractBaseEntityFactoryTest {
 
     @Test
     void createThrowsAnExceptionIfCopiesIsLessThanOne() {
-        BasicEntityFactory basicEntityFactory = new BasicEntityFactory(basicEntityRepository);
-
         Assertions.assertThrows(IllegalArgumentException.class, () -> basicEntityFactory.create(0));
+    }
+
+    void assertBasicEntityCorrectlyMade(final BasicEntity basicEntity) {
+        assertTrue(basicEntity instanceof BasicEntity);
+        assertNull(basicEntity.getId());
+        assertTrue(basicEntity.getMyLongAttribute() instanceof Long);
+        assertTrue(basicEntity.getMyStringAttribute() instanceof String);
+    }
+
+    void assertBasicEntityCorrectlyCreated(final BasicEntity basicEntity) {
+        assertTrue(basicEntity instanceof BasicEntity);
+        assertTrue(basicEntity.getId() instanceof Long);
+        assertTrue(basicEntity.getMyLongAttribute() instanceof Long);
+        assertTrue(basicEntity.getMyStringAttribute() instanceof String);
     }
 }
